0aa02a13beb0c275aff7e2f15a1373a7b814b945
Ivan Pascoe <sinh.rivera@gmail.com>
2023-01-19 15:01:28 -0600

    build grid fincioonal, enum with index

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,7 +12,29 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input()
+    |> pick_color
+    |> build_grid
+  end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = hex
+    |> Enum.chunk_every(3)
+    |> Enum.drop(-1)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten()
+    |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
